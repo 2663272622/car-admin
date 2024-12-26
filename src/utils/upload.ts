f3c4d4b97a8f83861814e253b341539bb5b49768
rock <2663272622@qq.com>
2024-12-26 09:25:22 +0800

    fix:upload

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -3,7 +3,14 @@
 // const OSS = require('ali-oss'); // 引入ali-oss依赖
 import OSS from 'ali-oss'
 export function client() {  
-    const client = ()=>{} 
+    const client = new OSS({
+        region: 'oss-cn-beijing',
+        refreshSTSTokenInterval: 300000,
+        accessKeyId: 'LTAI5tNd6WYEDWx52d88qmqt',
+        accessKeySecret: '05QdpVP18xzzcSawZhCz3R8vYQ2ik6',
+        bucket: 'img-ischool',
+        secure: true, 
+    })  
     return client
 }
  
